38c85f294c928c1de4aae7cd02704951fa03d188
Saimonn <simon.tourdot@linuxmail.org>
2021-07-12 23:41:25 +0200

    ADD & UPDATE(killsession & inside)
    Ajout fonction de déconnexion

--- inside.php
+++ inside.php
@@ -10,7 +10,8 @@ include('includes/db_call.php');
 
 <h2>Bon bon bon...pas grand chose à faire ici...</h2>
 <form action="index.php" method="post" class="login">
-    <input type="submit" value="Se déconnecter" />
+    <input type="hidden" value="killsession">
+    <input type="submit" value="Se déconnecter">
 </form>
 
 </html>
