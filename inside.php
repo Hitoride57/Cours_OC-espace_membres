ccbcf13a1b4bca10209c6935a25b69ac79141f51
Saimonn <simon.tourdot@linuxmail.org>
2021-07-21 23:30:56 +0200

    UPDATE(inside)
    Ajout d'une ligne pour tester la prise en compte de la session

--- inside.php
+++ inside.php
@@ -8,6 +8,8 @@ include('includes/db_call.php');
 
 ?>
 
+<h3 class="login">Salut <?php $_SESSION['pseudo'] ?> !</h3>
+
 <h2>Bon bon bon...pas grand chose à faire ici...</h2>
 <form action="index.php" method="post" class="login">
     <input type="hidden" value="killsession">
