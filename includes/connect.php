b7c841a032a0163ec29379aae77332907d2fd56f
Saimonn <simon.tourdot@linuxmail.org>
2021-07-21 23:32:09 +0200

    UPDATE(connect)
    Mise à jour test password pour éviter message d'erreur PHP en cas d'erreur de saisie

--- includes/connect.php
+++ includes/connect.php
@@ -11,33 +11,23 @@ if (isset($_POST['pseudo'])) {
     
     $resultat = $req->fetch();
         
-    // Comparaison des mots de passe hashés pour la connexion
-    $is_pass_correct = password_verify($_POST['password'], $resultat['pass']);
-
-    // Connexion si les identifiants sont corrects
-    if (!$resultat) {
+    // Connexion si les identifiants sont corrects et pass hashés identiques
+    if (!$resultat || !password_verify($_POST['password'], $resultat['pass'])) {
         echo '<h3 class="login">Tu t\'es gouré champion...</h3>';
     } else {
-        if ($is_pass_correct) {
-            session_start();
-            $_SESSION['id'] = $resultat['id'];
-            $_SESSION['pseudo'] = $pseudo;
+        session_start();
+        $_SESSION['id'] = $resultat['id'];
+        $_SESSION['pseudo'] = $pseudo;
+        $_SESSION['pass'] = $resultat['pass'];
 
-            // A voir si cette méthode est réellement sécurisée
-            if (isset($_POST['autologin'])) {
-                
-                $pass_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
-                
-                setcookie('id', $resultat['id'], time() + 3600, null, null, false, true);
-                setcookie('pseudo', $resultat['pseudo'], time() + 3600, null, null, false, true);
-                setcookie('pass', $pass_hash, time() + 3600, null, null, false, true);
-            }
+        // A voir si cette méthode est réellement sécurisée
+        if (isset($_POST['autologin'])) {
             
-            header('Location: inside.php');
-            
-        } else {
-            echo '<h3 class="login">Tu t\'es gouré champion...</h3>';
+            $_SESSION['autologin'];
         }
+        
+        header('Location: inside.php');
+        
     }
 
     $req->closeCursor();
